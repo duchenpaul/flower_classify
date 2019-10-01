05b1c2fe797dfeb126331e6b4654d38522009e4f
duchenpaul <duchenpaul@gmail.com>
2019-10-01 13:50:33 +0800

    Put parameters into config file

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
-DATASET_DIR = r'E:\python_test\machine_learning\training_dataset'
-DATASET_DIR = os.path.join(DATASET_DIR, '17flowers', 'jpg')
+import toolkit_config
+
+configDict = toolkit_config.read_config_general()['config']
+
+# DATASET_DIR = r'E:\python_test\machine_learning\training_dataset'
+# DATASET_DIR = os.path.join(DATASET_DIR, '17flowers', 'jpg')
+DATASET_DIR = configDict['dataset_dir']
 
 IMG_SIZE = 224
 
 DATA_DMP = 'dataset.npy'
-MODEL_NAME = 'flower_classify.model'
+# MODEL_NAME = 'flower_classify.model'
+MODEL_NAME = configDict['model_name']
