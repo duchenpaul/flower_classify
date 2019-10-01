24412bff7528541c02dc87533c469209f0c0de74
duchenpaul <duchenpaul@gmail.com>
2019-10-01 13:38:51 +0800

    Adjust path

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-DATASET_DIR = '17flowers/jpg'
-IMG_SIZE = 50
+import os
+DATASET_DIR = r'E:\python_test\machine_learning\training_dataset'
+DATASET_DIR = os.path.join(DATASET_DIR, '17flowers', 'jpg')
+
+IMG_SIZE = 224
 
 DATA_DMP = 'dataset.npy'
 MODEL_NAME = 'flower_classify.model'
